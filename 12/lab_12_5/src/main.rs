88e179d9b2d234c2a33faca71124c8398039a5db
Jost221 <mr.z.75@mail.ru>
2023-04-29 16:36:41 +0300

    продолжу позже, пора в кванториум

--- 12/lab_12_5/src/main.rs
+++ 12/lab_12_5/src/main.rs
@@ -1,6 +1,12 @@
 use std::fs::File;
+use std::io::Read;
 
-fn main() -> Result<()>{
-    let mut file = File::open("img.bmp");
-    println!("{}", file.read().unwrap())?;
+fn main() -> std::io::Result<()>{
+    let mut file = File::open("img.bmp")?;
+    let metadata = file.metadata()?;
+    let len_buf = (metadata.len()+1) as usize;
+    let mut buffer = vec![0; len_buf];
+    file.read(&mut buffer)?;
+    println!("{:?}", buffer);
+    Ok(())
 }
