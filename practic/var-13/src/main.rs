d827ec8785804f7c1f180a14dc9458c25a21523f
Jost221 <mr.z.75@mail.ru>
2023-05-16 12:37:34 +0300

    var 13 num 1 is redy

--- practic/var-13/src/main.rs
+++ practic/var-13/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut a:f32 = 3.4;
+    let mut b:f32 = 4.6;
+    print_table(a.clone(), b.clone())
+}
+
+fn print_table(mut a:f32, b: f32){
+    println!(" x  y");
+    let mut i = 0;
+    while a <= b{
+        println!("{:2} {:.1}", i, a);
+        a+=0.1;
+        i +=1;
+    }
 }
